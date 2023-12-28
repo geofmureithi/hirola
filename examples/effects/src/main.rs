fd2d276539947e61e60324a99b9105343de4d72b
Njuguna Mureithi <mureithinjuguna@gmail.com>
2023-12-28 22:54:27 +0300

    add: effects examples

---
+++ examples/effects/src/main.rs
@@ -0,0 +1,48 @@
+use hirola::{
+    dom::{effects::prelude::*, mount, Dom},
+    prelude::*,
+};
+
+fn effects_demo() -> Dom {
+    let is_shown = Mutable::new(true);
+    let duration = Mutable::new(800u64);
+    html! {
+        <form
+            use:future=async {
+                // similar to on mount
+            }
+            class="h-screen flex flex-col items-center justify-center transition-all ease-in-out delay-1000"
+        >
+            <span x:html="<strong>calebporzio</strong>" /> // Raw html mixin
+            <input
+                type="range"
+                bind:value=&duration // Two way binding
+                use:signal=attr_signal("data-value", duration.signal()) // Generic signal binding
+                max=1000
+                step=100
+            />
+            <div class="base">
+                // Directly include a reactive
+                <h1>{duration}</h1>
+
+                <p>"Shown: "{is_shown.clone()}</p>
+
+                <button
+                    class="bg-gray-200 mt-4 font-bold py-2 px-4 rounded"
+                    // Events are prefixed with on:
+                    on:click=move |e| {
+                        e.prevent_default();
+                        *is_shown.lock_mut() = !is_shown.get();
+                    }
+                >
+                    "Click Me"
+                </button>
+            </div>
+        </form>
+    }
+}
+
+fn main() {
+    console_error_panic_hook::set_once();
+    mount(effects_demo()).unwrap();
+}
